c2f0517743091405b4e7e8d3c1b5bda6650bac9c
Max Girteit <icufishmg@gmail.com>
2022-04-26 08:48:30 -0600

    moved onClick to button instead of entire alert

--- frontend/src/components/alerts/AlertBuilderToken.js
+++ frontend/src/components/alerts/AlertBuilderToken.js
@@ -8,13 +8,17 @@ import theme from '../../Styles';
 
 export default function AlertBuilderToken(props) {
   return (
-    <Stack sx={{ cursor: 'pointer', width: '100%' }} spacing={2}>
-      <Alert onClick={() => props.modalControl()} severity='warning'>
+    <Stack sx={{ width: '100%' }} spacing={2}>
+      <Alert severity='warning'>
         <AlertTitle sx={{ fontWeight: 'bold' }}>
           You must own at least 1 Builder Token to mint a Visible record.
         </AlertTitle>
         Owning a Builder Token NFT grants access to all Visible services.{' '}
-        <Button sx={{ ml: theme.spacing(3) }} variant='contained'>
+        <Button
+          onClick={() => props.modalControl()}
+          sx={{ ml: theme.spacing(3) }}
+          variant='contained'
+        >
           Get a Builder Token
         </Button>
       </Alert>
