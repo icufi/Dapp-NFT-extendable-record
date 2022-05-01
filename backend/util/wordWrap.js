46d564cf9897fd837c562a8b9abd810b26723e9b
Max Girteit <icufishmg@gmail.com>
2022-05-01 16:58:36 -0600

    changed variable type

--- backend/util/wordWrap.js
+++ backend/util/wordWrap.js
@@ -1,19 +1,17 @@
-
-
 function wordWrap(str, len) {
-    let input = str.trim().split(' ');
-    let [index, output] = [0, []]
-    output[index] = '';
-    input.forEach(word => {
-        let temp = `${output[index]} ${word}`.trim()
-        if (temp.length <= len) {
-            output[index] = temp;
-        } else {
-            index++;
-            output[index] = word;
-        }
-    })
-    return output
+  const input = str.trim().split(' ');
+  let [index, output] = [0, []];
+  output[index] = '';
+  input.forEach((word) => {
+    const temp = `${output[index]} ${word}`.trim();
+    if (temp.length <= len) {
+      output[index] = temp;
+    } else {
+      index += 1;
+      output[index] = word;
+    }
+  });
+  return output;
 }
 
 module.exports = wordWrap;
