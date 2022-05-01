af66a0e8a2a61adeea3af537e15f23640e9c97a5
Max Girteit <icufishmg@gmail.com>
2022-05-01 16:14:44 -0600

    removed unused variables

--- backend/events/emailInterval.js
+++ backend/events/emailInterval.js
@@ -5,16 +5,15 @@ const Email = require('../models/email');
 const Record = require('../models/record');
 const emailTemplate = require('../assets/email/emailTemplate');
 
-const sleep = (milliseconds) => {
-  return new Promise((resolve) => setTimeout(resolve, milliseconds));
-};
+const sleep = (milliseconds) =>
+  new Promise((resolve) => setTimeout(resolve, milliseconds));
 
-//emailInterval sends a user generated email from db at timed interval if both a prtoken and mrrtoken have been minted.
+// emailInterval sends a user generated email from db at timed interval if both a prtoken and mrrtoken have been minted.
 exports.emailInterval = async () => {
   setInterval(async () => {
     console.log('email interval fired');
 
-    //get emails in db by pending status true
+    // get emails in db by pending status true
     let emails;
     try {
       emails = await Email.find({ emailPending: true });
@@ -25,21 +24,22 @@ exports.emailInterval = async () => {
     console.log('email array:', emails);
     console.log('number of pending emails:', emails.length);
 
-    //if no pending true emails, return
+    // if no pending true emails, return
     if (!emails || emails.length === 0) {
       console.log('No pending emails in db.');
       return;
     }
 
-    //loop through pending emails.  chekck validity. send if valid.
+    // loop through pending emails. send if valid.
+    // eslint-disable-next-line no-restricted-syntax
     for (const email of emails) {
-      //exit if email is not pending.
+      // exit if email is not pending.
       if (email.emailPending === false) {
         console.log('Email is not pending.');
         continue;
       }
 
-      //slow down loop because deals with several network calls
+      // slow down loop because deals with several network calls
       sleep(5000);
 
       if (email.emailCompleteTime) {
@@ -47,7 +47,7 @@ exports.emailInterval = async () => {
         continue;
       }
 
-      //if email pending longer than 24 hours, set pending to false, timeout to true
+      // if email pending longer than 24 hours, set pending to false, timeout to true
       const currentTime = Date.now();
       if (email.createTime - currentTime > 86400000) {
         try {
@@ -61,7 +61,7 @@ exports.emailInterval = async () => {
         }
       }
 
-      //call record from db by email dna
+      // call record from db by email dna
       let dbRecord;
       if (!email.mrrTokenId) {
         try {
@@ -72,7 +72,7 @@ exports.emailInterval = async () => {
         }
       }
 
-      //if record by email dna does not return a single record, continue
+      // if record by email dna does not return a single record, continue
       if (!dbRecord || dbRecord.length !== 1) {
         console.log(
           'Record is not unique by email dna or dna does not exist in records db.'
@@ -80,35 +80,34 @@ exports.emailInterval = async () => {
         continue;
       }
 
-      //extract first array object, there should be only one
-      dbRecord = dbRecord[0];
+      // extract first array object, there should be only one
+      dbRecord = [dbRecord];
 
-      //if no prTokenId then record was never fully minted, continue
+      // if no prTokenId then record was never fully minted, continue
       if (!dbRecord.prTokenId) {
         console.log('prTokenId is not in db.');
         continue;
       }
 
-      //if not mrrTokenId then record was not full minted, continue
+      // if not mrrTokenId then record was not full minted, continue
       if (!dbRecord.mrrTokenId) {
         console.log('mrrTokenId has not been minted.');
         continue;
       }
 
-      //if prTokenId does not match mrrTokenId then mint of record was corrupt
+      // if prTokenId does not match mrrTokenId then mint of record was corrupt
       if (dbRecord.mrrTokenId != dbRecord.prTokenId) {
         console.log('Tokens do not match at record database.');
         continue;
       }
 
-      //if checks passed, go to send email
+      // if checks passed, go to send email
 
-      //email transporter object
+      // email transporter object
 
       const options = {
         auth: {
-          api_key:
-            process.env.EMAIL_API,
+          api_key: process.env.EMAIL_API,
         },
       };
 
@@ -118,14 +117,9 @@ exports.emailInterval = async () => {
 
       console.log('dbRecord mrrTokenId:', dbRecord.mrrTokenId);
 
-      const image = email.image;
-      const nftTokenType = email.nftTokenType;
-      const nftTokenId = email.nftTokenId;
-      const message = email.message;
-      const replyAddress = email.emailReply ? email.emailReply : '';
+      const { image, nftTokenType, nftTokenId, message, replyAddress } = email;
 
-      //send mail
-      let emailReceipt;
+      // send mail
       try {
         mailer.sendMail(
           {
@@ -140,21 +134,21 @@ exports.emailInterval = async () => {
               replyAddress
             ),
           },
-          async function (err, res) {
+          async (err, res) => {
             if (err) {
               console.log('Email send failed.', err);
             }
             console.log(res);
             try {
-              //update email status and mrrTokenId, save to db
+              // update email status and mrrTokenId, save to db
               email.emailPending = false;
               email.emailCompleteTime = Date.now();
               email.mrrTokenId = dbRecord.mrrTokenId;
               await email.save();
-            } catch (err) {
+            } catch (error) {
               console.log(
                 'Failed to update email complete and pending status to db.',
-                err
+                error
               );
             }
           }
