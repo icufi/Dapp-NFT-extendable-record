ffc29eb3a7f5891de49e5b9b9ea6bf91fd12a9b6
Max Girteit <icufishmg@gmail.com>
2022-05-01 20:43:39 -0600

    changed import method for templates

--- backend/assets/templates/BAYC/BAYCMaster.js
+++ backend/assets/templates/BAYC/BAYCMaster.js
@@ -1,13 +1,13 @@
-const BAYCCREAMx836f6989 = require('./BAYCCREAMx836f6989');
-const BAYCGreenwichx836f6989 = require('./BAYCGreenwichx836f6989');
-const BAYCOG1PRBlackx836f6989 = require('./BAYCOG1PRBlackx836f6989');
-const BAYCgreenmilx836f6989 = require('./BAYCgreenmilx836f6989');
+const { BAYCCREAMx836f6989 } = require('./BAYCCREAMx836f6989');
+const { BAYCGreenwichx836f6989 } = require('./BAYCGreenwichx836f6989');
+const { BAYCOG1PRBlackx836f6989 } = require('./BAYCOG1PRBlackx836f6989');
+const { BAYCgreenmilx836f6989 } = require('./BAYCgreenmilx836f6989');
 
-const BAYCnewbluex836f6989 = require('./BAYCnewbluex836f6989');
-const BAYCgreyx836f6989 = require('./BAYCgreyx836f6989');
-const BAYCbluex836f6989 = require('./BAYCbluex836f6989');
-const BAYCpurplex836f6989 = require('./BAYCpurplex836f6989');
-const BAYCaquamarinex836f6989 = require('./BAYCaquamarinex836f6989');
+const { BAYCnewbluex836f6989 } = require('./BAYCnewbluex836f6989');
+const { BAYCgreyx836f6989 } = require('./BAYCgreyx836f6989');
+const { BAYCbluex836f6989 } = require('./BAYCbluex836f6989');
+const { BAYCpurplex836f6989 } = require('./BAYCpurplex836f6989');
+const { BAYCaquamarinex836f6989 } = require('./BAYCaquamarinex836f6989');
 
 module.exports = [
   BAYCOG1PRBlackx836f6989,
